c005ffe8d8d878f74e10427d9204384943ff4ab5
Gerardo Garcia <149715796+Geemoney64789@users.noreply.github.com>
2024-06-27 10:39:12 -0700

    updating index.js
    update

---
+++ index.js
@@ -0,0 +1,37 @@
+//variables
+const courseInfo = {
+    "id": 30,
+    "name": "Introduction to Programming"
+};
+
+const assignmentGroup = {
+    "id": 10,
+    "name": "Fundamentals of JavaScript",
+   // the ID of the course the assignment group belongs to
+"course_id": 30,
+// the percentage weight of the entire assignment group
+"group_weight": 22,
+"assignments": [AssignmentInfo],
+}
+
+        {
+            "id": 777,
+            "name": "Create a Function"
+            // the due date for the assignment
+            "due_at": 10-10-2028,
+            "points_possible": 100,
+        },
+        {
+            "id": 888,
+            "name": ""
+            "due_at": 10-01-2028,
+            "points_possible": 100,
+        },
+        {
+            "id": 999,
+            "name": "Create a To-Do List"
+            "due_at": 10-01-2028,
+            "points_possible": 100,
+        }
+    ]
+}
